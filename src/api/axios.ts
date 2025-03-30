808510a1aced7d3d41c4c15a91bfd3bfc55621aa
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-30 19:40:16 +0530

    base url for production

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api";
+// const BASE_URL = "http://localhost:3000/api";
+
+const BASE_URL_PRODUCTION =
+    "https://app-crafter-backend-production.up.railway.app/api";
 
 const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL_PRODUCTION,
     headers: {
         "Content-Type": "application/json",
     },
