c0f5fa2917e4b5e3fc95816f38acccd93d2a80e6
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-30 20:12:03 +0530

    update:deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,6 @@ export default defineConfig({
     server: {
         host: true,
         port: Number(process.env.PORT) || 5173,
+        allowedHosts: ["appcrafter-react.onrender.com"],
     },
 });
