02889f2ff866c724a9b20f10244e7fcacfa1cb32
zee0401 <41227625+zee0401@users.noreply.github.com>
2025-03-30 13:40:42 +0530

    feat: axios installed , api function created for crud operation

---
+++ src/api/api.ts
@@ -0,0 +1,32 @@
+import api from "./axios";
+import { ResourceType } from "@/types/resourcesTypes";
+
+export const getAllResources = async (): Promise<ResourceType[]> => {
+    const response = await api.get("/all-resources");
+    return response.data.resources;
+};
+
+// export const getResourceById = async (id: string) => {
+//     const response = await api.get(`/resources/${id}`);
+//     return response.dataS;
+// };
+
+export const createResource = async (
+    resource: ResourceType
+): Promise<ResourceType> => {
+    const response = await api.post("/add-resources", resource);
+    return response?.data?.resources;
+};
+
+export const updateResource = async (
+    id: string,
+    resource: Partial<ResourceType>
+): Promise<ResourceType> => {
+    const response = await api.put(`/update-resources/${id}`, resource);
+    return response.data;
+};
+
+export const deleteResource = async (id: string): Promise<void> => {
+    const response = await api.delete(`/delete-resources/${id}`);
+    return response.data;
+};
